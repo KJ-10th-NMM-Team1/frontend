9bb20a02399ba293718f9cdd000591e3512e983f
kimhyunso <gustn9515@gmail.com>
2025-11-06 21:35:49 +0900

    fix: 배포 스크립트 수정

--- deploy/start_server.sh
+++ deploy/start_server.sh
@@ -5,11 +5,6 @@ APP_DIR="/home/ubuntu/app"
 cd $APP_DIR
 
 echo "Starting React SSR (Node.js) server using pm2..."
-<<<<<<< HEAD
 pm2 delete react-app || true
 pm2 start npm --name "react-app" -- run dev
 echo "Life Cycle - ApplicationStart: Server successfully started"
-=======
-pm2 start npm --name "react-app" -- run dev
-echo "Life Cycle - ApplicationStart: Server successfully started"
->>>>>>> 82be241 (Deploy (#81))
